0c429427fa2e0272bc62cb615302be64012683bd
bakelgithub <lukas.koerber@gmx.net>
2025-01-06 20:51:58 +0100

    Lk - refactored CallToAction

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,17 +1,10 @@
-import { Center, useBreakpointValue } from "@chakra-ui/react";
-import { useContaxt } from "../context/Context";
+import { Center } from "@chakra-ui/react";
 import { ActionButton } from "./ActionButton";
 
 export function CallToAction() {
-  const { spacingTop } = useContaxt();
-  const text = useBreakpointValue({
-    base: "Termin vereinbaren",
-    sm: "Jetzt Termin vereinbaren",
-    md: "Termin vereinbaren",
-  });
   return (
     <Center>
-      <ActionButton text={text} />
+      <ActionButton text={"kostenloses Kennenlerngespräch"} />
     </Center>
   );
 }
